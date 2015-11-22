c6ea3db029a35bb98114a1636895ff2f58369870
Remco Bouckaert <higgs.ml@gmail.com>
2015-11-23 08:42:51 +1300

    add BEASTModelTest class for backward compatibilty

---
+++ src/beast/evolution/sitemodel/BEASTModelTest.java
@@ -0,0 +1,9 @@
+package beast.evolution.sitemodel;
+
+import beast.core.Description;
+
+@Description("Used for backward compatibiliy only, so files generated with previous versions of modeltest can still run:")
+@Deprecated
+public class BEASTModelTest extends BEASTModelTestSiteModel {
+
+}
