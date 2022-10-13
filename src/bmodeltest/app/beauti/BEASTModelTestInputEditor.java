ce0f9d5dd389c1e2623d80b2fafc63e2c87b8f7d
rbouckaert <higgs.ml@gmail.com>
2022-10-13 14:34:16 +1300

    fix some BEAUti behaviour

--- src/bmodeltest/app/beauti/BEASTModelTestInputEditor.java
+++ src/bmodeltest/app/beauti/BEASTModelTestInputEditor.java
@@ -2,12 +2,17 @@ package bmodeltest.app.beauti;
 
 
 import beastfx.app.inputeditor.BeautiDoc;
+import beastfx.app.inputeditor.BeautiPanel;
 import beastfx.app.inputeditor.BooleanInputEditor;
 import beastfx.app.inputeditor.EnumInputEditor;
 import beastfx.app.inputeditor.SiteModelInputEditor;
 import bmodeltest.evolution.sitemodel.BEASTModelTestSiteModel;
 import bmodeltest.evolution.substitutionmodel.ModelFrequencies;
 import bmodeltest.evolution.substitutionmodel.NucleotideRevJumpSubstModel;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import beast.base.core.BEASTInterface;
 import beast.base.core.Input;
 import beast.base.evolution.substitutionmodel.SubstitutionModel;
@@ -25,20 +30,28 @@ public class BEASTModelTestInputEditor extends SiteModelInputEditor {
 
 	@Override
 	public void init(Input<?> input, BEASTInterface plugin, int itemNr, ExpandOption bExpandOption, boolean bAddButtons) {
-		super.init(input, plugin, itemNr, bExpandOption, bAddButtons);
+		super.init(input, plugin, itemNr, ExpandOption.TRUE, bAddButtons);
+		
 		SubstitutionModel sm = ((BEASTModelTestSiteModel) input.get()).substModelInput.get();
 		NucleotideRevJumpSubstModel substModel = (NucleotideRevJumpSubstModel) sm;
 		EnumInputEditor typeEditor = new EnumInputEditor(doc);
 		typeEditor.init(substModel.modelChoiseInput, substModel, itemNr, bExpandOption, bAddButtons);
 		
-		pane.getChildren().add(typeEditor);
-		// ((Box) getComponent(0)).add(typeEditor, 2);
+		Pane p = (Pane) pane.getChildren().get(0);
+		p = (Pane)p.getChildren().get(0);
+		Label label = new Label("Model set: ");
+		label.setMinSize(205, 20);
+		label.setPadding(new Insets(5));
+		p.getChildren().add(new HBox(label, typeEditor));
 		
 		ModelFrequencies freqs = (ModelFrequencies) substModel.frequenciesInput.get();
 		BooleanInputEditor equalFreqs = new BooleanInputEditor(doc);
 		equalFreqs.init(freqs.empiricalInput, freqs, itemNr, bExpandOption, bAddButtons);
-		pane.getChildren().add(typeEditor);
-		// ((Box) getComponent(0)).add(equalFreqs, 3);
+		Label label2 = new Label("Frequencies: ");
+		label2.setMinSize(205, 20);
+		label2.setPadding(new Insets(5));
+		p.getChildren().add(new HBox(label2, equalFreqs));
+
 		validateInput();
 	}
 	
--- src/bmodeltest/app/beauti/NucleotideRevJumpSubstModelRatePriorInputEditor.java
+++ src/bmodeltest/app/beauti/NucleotideRevJumpSubstModelRatePriorInputEditor.java
@@ -2,11 +2,14 @@ package bmodeltest.app.beauti;
 
 
 
+
 import beastfx.app.inputeditor.BeautiDoc;
 import beastfx.app.inputeditor.EnumInputEditor;
 import beastfx.app.inputeditor.InputEditor;
+import beastfx.app.util.FXUtils;
 import bmodeltest.math.distributions.NucleotideRevJumpSubstModelRatePrior;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import beast.base.core.BEASTInterface;
 import beast.base.core.Input;
 
@@ -24,11 +27,14 @@ public class NucleotideRevJumpSubstModelRatePriorInputEditor extends InputEditor
 	@Override
 	public void init(Input<?> input, BEASTInterface beastObject, int itemNr, ExpandOption isExpandOption,
 			boolean addButtons) {
-		pane.getChildren().add(new Label(beastObject.getID() + ":"));
+		pane = FXUtils.newHBox();
 		
 		EnumInputEditor editor = new EnumInputEditor(doc);
 		editor.init(beastObject.getInput("priorType"), beastObject, -1, isExpandOption, true);
+		Label label = (Label)((Pane)editor.getChildren().get(0)).getChildren().get(0);
+		label.setText(beastObject.getID() + ": Prior type");
 		pane.getChildren().add(editor);
+		getChildren().add(pane);
 	}
 
 }
